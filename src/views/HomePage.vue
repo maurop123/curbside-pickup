6c909002468e20898137a5d72cb61a74d9bc1c8c
Mauro P <maurop123@gmail.com>
2024-03-27 00:03:40 -0400

    add prettier add google font. apply to title

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,74 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+	<ion-page>
+		<ion-header :translucent="true">
+			<ion-toolbar>
+				<ion-title>{{ navTitle }}</ion-title>
+			</ion-toolbar>
+		</ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+		<ion-content :fullscreen="true">
+			<ion-header collapse="condense">
+				<ion-toolbar>
+					<ion-title size="large">{{ navTitle }}</ion-title>
+				</ion-toolbar>
+			</ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+			<div id="container">
+				<strong>Ready to create an app?</strong>
+				<p>
+					Start with Ionic
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://ionicframework.com/docs/components"
+						>UI Components</a
+					>
+				</p>
+			</div>
+		</ion-content>
+	</ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+	import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'
+
+	const navTitle = 'Curbside Pickup'
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+	ion-title {
+		font-family: 'Lilita One', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+		font-size: 2rem;
+	}
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+	/* Default */
+	#container {
+		text-align: center;
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 50%;
+		transform: translateY(-50%);
+	}
 
-#container a {
-  text-decoration: none;
-}
+	#container strong {
+		font-size: 20px;
+		line-height: 26px;
+	}
+
+	#container p {
+		font-size: 16px;
+		line-height: 22px;
+
+		color: #8c8c8c;
+
+		margin: 0;
+	}
+
+	#container a {
+		text-decoration: none;
+	}
 </style>

f4a9cc9dd3f95e550c6dc8800672ee7da02aec41
Mauro P <maurop123@gmail.com>
2024-04-01 21:38:57 -0400

    capture still image

--- src/components/NewPostModal.vue
+++ src/components/NewPostModal.vue
@@ -1,26 +1,51 @@
 <template>
 	<ion-content>
-		<video ref="camera">
-			<source src="" />
-		</video>
+		<ion-grid>
+			<ion-row>
+				<video ref="camera">
+					<source src="" />
+				</video>
+			</ion-row>
+			<ion-row class="ion-justify-content-center">
+				<ion-button @click="capture()"> Capture </ion-button>
+			</ion-row>
+			<ion-row>
+				<img ref="stillImg" />
+				<canvas ref="canvas" />
+			</ion-row>
+		</ion-grid>
 	</ion-content>
 </template>
 
 <script setup lang="ts">
 	import { ref, onMounted } from 'vue'
-	import { IonContent } from '@ionic/vue'
+	import { IonButton, IonContent } from '@ionic/vue'
 
 	const camera = ref(null)
+	const canvas = ref(null)
+	const stillImg = ref(null)
+	let videoStream = null
 	onMounted(() => {
 		// Camera
 		console.debug('camera', camera)
 		const videoDevice = navigator?.mediaDevices?.getUserMedia({ video: true })
 		if (videoDevice)
 			videoDevice.then(stream => {
-				camera.value.srcObject = stream
+				videoStream = stream
+				camera.value.srcObject = videoStream
 				camera.value.play()
 			})
+
+		// Canvas
+		console.debug('canvas', canvas)
 	})
+
+	// Capture
+	function capture() {
+		console.debug('capturing', videoStream)
+		const context = canvas.value.getContext('2d')
+		context.drawImage(camera.value, 0, 0, 200, 150)
+	}
 </script>
 
 <style scoped></style>

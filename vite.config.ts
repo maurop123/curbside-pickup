e1cd54002350066c3dfc9dd7f37d95cbfa48ef32
Mauro P <maurop123@gmail.com>
2024-04-17 17:23:51 -0600

    fix build config using vite.config instead of vue.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,15 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    base: '/curbside-pickup',
+    plugins: [vue(), legacy()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
 })

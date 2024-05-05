e9950c0fb50d480b389fc2a9ca5f510b9a13fc06
Mauro P <maurop123@gmail.com>
2024-05-05 14:09:39 -0600

    default light theme

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -3,16 +3,29 @@
         <ion-title slot="start">{{ navTitle }}</ion-title>
         <ion-buttons slot="end">
             <ion-button>
-                <ion-icon slot="icon-only" :icon="personCircle"></ion-icon>
+                {{ loginText }}
+                <ion-icon :icon="personCircle"></ion-icon>
             </ion-button>
         </ion-buttons>
     </ion-toolbar>
 </template>
 
 <script setup lang="ts">
+    import { ref, computed } from 'vue'
     import { IonButton, IonButtons, IonIcon, IonTitle, IonToolbar } from '@ionic/vue'
     import { personCircle } from 'ionicons/icons'
+    import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+
+    //app
     const navTitle: string = 'Curbside Pickup'
+    //vuefire
+    const auth = useFirebaseAuth()
+    const authError = ref(null)
+    const loginText = computed(() => {
+        return useCurrentUser() === null ? 'login' : 'logout'
+    })
+
+    console.debug('useCurrentUser', useCurrentUser().value)
 </script>
 
 <style scoped></style>
